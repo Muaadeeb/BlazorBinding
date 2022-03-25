7cb4d090918263e74498c266f19f3089824e8938
Timothy Ingledue <Timothy.Ingledue@Orion.com>
2022-03-25 09:13:34 -0500

    0000 - checkin

---
+++ Models/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public double Price { get; set; }
+
+        public List<ProductProp> ProductProperties { get; set; } = null!;
+    }
+}
