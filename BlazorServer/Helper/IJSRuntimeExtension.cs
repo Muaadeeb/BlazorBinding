6f8ffd970a28d94b11f5bad1fa1666c6ae90f50c
Timothy Ingledue <Timothy.Ingledue@Orion.com>
2022-04-12 11:57:28 -0500

    Routing and LifeCycle events - also parent invoking method on child component.

---
+++ BlazorServer/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazorServer.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jsRunTime, string message)
+        {
+            await jsRunTime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ BlazorServer/wwwroot/js/common.js
@@ -0,0 +1,34 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful");
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed");
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        )
+
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
